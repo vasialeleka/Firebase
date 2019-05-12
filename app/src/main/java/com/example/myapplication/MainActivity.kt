8c6be49d879ecf0376014bd8b9dd6926e6807af5
Vasia <vasialeleka@ukr.net>
2019-05-12 17:06:46 +0300

    edit item note style

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -17,6 +19,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     var maxId: Int = 0
     lateinit var notes: String
     var listOfNotes = mutableListOf<Note>()
+    lateinit var context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +27,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setSupportActionBar(toolbar)
 
         FirebaseApp.initializeApp(this)
+        context = this
         ref = FirebaseDatabase.getInstance().getReference().child("Note")
         ref.addValueEventListener(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
@@ -31,12 +35,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
             override fun onDataChange(p0: DataSnapshot) {
                 maxId = p0.childrenCount.toInt()
-                for (item in 0..maxId-1) {
-                    if (!p0.child(item.toString()).child("text").toString().isEmpty() || p0.child(item.toString()).child("text").toString().length !=0 ) {
+                for (item in 0..maxId - 1) {
+                    if (!p0.child(item.toString()).child("text").toString().isEmpty() || p0.child(item.toString()).child("text").toString().length != 0) {
                         listOfNotes.add(Note(p0.child(item.toString()).child("title").getValue().toString(), p0.child(item.toString()).child("text").getValue().toString()))
+                        recView.layoutManager = LinearLayoutManager(context)
+                        recView.adapter = NoteAdapter(listOfNotes, context)
                     }
                 }
-                var s1 = "";
+
 
             }
         })
