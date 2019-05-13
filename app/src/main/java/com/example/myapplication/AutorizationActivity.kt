d66f12f6e1984271eb5b5b90605f1f7bae4d9e37
Vasia <vasialeleka@ukr.net>
2019-05-13 20:32:16 +0300

    add registration method

---
+++ app/src/main/java/com/example/myapplication/AutorizationActivity.kt
@@ -0,0 +1,59 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_autorization.*
+import kotlinx.android.synthetic.main.app_bar.*
+
+class AutorizationActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var mAuth: FirebaseAuth
+    lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+    lateinit var context: Context
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_autorization)
+        context = context
+        mAuth = FirebaseAuth.getInstance()
+        btnBack.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            btnBack -> super.onBackPressed()
+            btnRegistr -> regisration()
+            btnLogIn -> logIn()
+        }
+    }
+
+    private fun logIn() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun regisration() {
+        if (txtEmail.toString().isEmpty()) {
+            Toast.makeText(context, "Enter email", Toast.LENGTH_SHORT).show()
+        } else if (txtPassword.toString().isEmpty()) {
+            Toast.makeText(context, "Enter password", Toast.LENGTH_SHORT).show()
+        } else {
+            mAuth.signInWithEmailAndPassword(txtEmail.toString(), txtPassword.toString())
+                .addOnCompleteListener(this, object : OnCompleteListener<AuthResult> {
+                    override fun onComplete(task: Task<AuthResult>) {
+                        if (task.isSuccessful) {
+                            Toast.makeText(context, "Done", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(context, "Done", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
+        }
+    }
+
+}
